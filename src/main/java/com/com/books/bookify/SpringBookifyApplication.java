cf884e00bb6eec1442dacac9ccd63a2211abb01d
@mohdarif8299 <mohdarif8299@gmail.com>
2020-03-14 00:37:29 +0530

    First COmmit

--- src/main/java/com/com/books/bookify/SpringBookifyApplication.java
+++ src/main/java/com/com/books/bookify/SpringBookifyApplication.java
@@ -2,7 +2,9 @@ package com.com.books.bookify;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 import com.com.books.bookify.storage.StorageService;
 
@@ -10,10 +12,13 @@ import com.com.books.bookify.storage.StorageService;
 @EnableConfigurationProperties({
     StorageService.class
 })
-public class SpringBookifyApplication {
+public class SpringBookifyApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBookifyApplication.class, args);
 	}
-
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringBookifyApplication.class);
+	}
 }
